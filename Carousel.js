79270577b61dcb064f0b3ee1353f52f762ec995f
Anoop <anoop07.1987@gmail.com>
2015-08-16 17:04:38 -0400

    Create Carousel.js

---
+++ Carousel.js
@@ -0,0 +1,34 @@
+var showImage = function (x){
+    $("[class^=image").hide();
+    $("[class^=sliderButton").removeClass("button-on");
+    $("[class^=sliderButton").addClass("button-off");
+    $(".image" + x).show();
+    $(".sliderButton" + x).addClass("button-on");
+    $(".sliderButton" + x).removeClass("button-off");
+}
+var createCarousel = function(images){
+    var counter=0;
+    var imageHtml = "";
+    var sliderHtml = "<div class=\"slider\">";
+    while (counter < images.length){
+        imageHtml = imageHtml + 
+        "<div class=\"imageDiv image" + counter +"\">"+
+            "<image class=\"img\" src=\""+ images[counter] +"\" />"+
+        "</div>";
+        sliderHtml = sliderHtml + 
+        "<div class=\"sliderButton sliderButton" + counter +
+        "\" id=\"sliderButton" + counter +
+        "\" onclick=\"showImage(" + counter + ")\"></div>";
+        counter++;
+    }
+
+    sliderHtml = sliderHtml + "</div>";
+    $(".canvas").append(imageHtml);
+    $(".canvas").append(sliderHtml);
+    var offset = ($(".slider").width())/(images.length + 1);
+    for (var i=0;i<images.length;i++){
+            $(".sliderButton"+i).css("left", offset * (i+1) +  "px");
+    }
+    showImage(0);
+	
+};
